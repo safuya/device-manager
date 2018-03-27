fe3283b10f8dba94f733a8b65402acbd768a45fa
Robert Hughes <roberts.ginger.email@gmail.com>
2018-03-27 09:54:25 +0100

    add devices

--- app/controllers/device_controller.rb
+++ app/controllers/device_controller.rb
@@ -3,4 +3,21 @@ class DeviceController < ApplicationController
     @devices = Device.all
     erb :'devices/index'
   end
+
+  get '/devices/new' do
+    @groups = Group.all
+    erb :'devices/new'
+  end
+
+  post '/devices' do
+    device = Device.new(params[:device])
+    device.save
+    redirect "/devices/#{device.id}"
+  end
+
+  get '/devices/:id' do
+    @device = Device.find(params[:id])
+    @groups = @device.groups
+    erb :'devices/show'
+  end
 end
--- spec/controllers/device_controller_spec.rb
+++ spec/controllers/device_controller_spec.rb
@@ -27,4 +27,65 @@ describe 'DeviceController' do
       expect(page.body).to include("href=\"/devices/#{@hub.id}\"")
     end
   end
+
+  describe '/devices/:id' do
+    before do
+      @hub = Device.create(serial_number: '+012345+0123456789',
+                           model: 'HH6A',
+                           firmware_version: 'ABC123',
+                           last_contact: Time.now,
+                           last_activation: Time.now - 54_432_000)
+      @admin = Group.create(name: 'admin', privilege: 'admin')
+      @write = Group.create(name: 'room 210 lead', privilege: 'write')
+      @hub.groups = [@admin, @write]
+      @hub.save
+    end
+
+    it 'allows you to view an individual Hub' do
+      visit "/devices/#{@hub.id}"
+      expect(page.body).to include(@hub.serial_number)
+      expect(page.body).to include(@hub.model)
+      expect(page.body).to include(@hub.firmware_version)
+      expect(page.body).to include(@hub.last_contact.to_s)
+      expect(page.body).to include(@hub.last_activation.to_s)
+
+      expect(page.body).to include(@admin.name)
+      expect(page.body).to include(@write.name)
+    end
+
+    it 'links to the groups' do
+      visit "/devices/#{@hub.id}"
+      expect(page.body).to include("href=\"/groups/#{@admin.id}\"")
+    end
+  end
+
+  describe '/devices/new' do
+    before do
+      @admin = Group.create(name: 'admin', privilege: 'admin')
+      @write = Group.create(name: 'room 210 lead', privilege: 'write')
+    end
+
+    it 'allows you to add a new device' do
+      visit '/devices/new'
+      expect(page.body).to include('<form')
+      expect(page.body).to include('device[serial_number]')
+      expect(page.body).to include('device[model]')
+      expect(page.body).to include('device[last_contact]')
+      expect(page.body).to include('device[last_activation]')
+      expect(page.body).to include('device[groups][]')
+      expect(page.body).to include('submit')
+    end
+
+    it 'creates a new device' do
+      serial_number = '+065432+0123456789'
+      visit '/devices/new'
+      fill_in :serial_number, with: serial_number
+      fill_in :model, with: 'HH6A'
+      fill_in :last_contact, with: '180203T11:24'
+      fill_in :last_activation, with: '160203T10:09'
+      click_button 'Add Device'
+      expect(Device.find_by(serial_number: '+065432+0123456789').model)
+        .to eql('HH6A')
+    end
+  end
 end
