bb3cb7502b25cabce5e005008dbc854c61139d6b
Robert Hughes <roberts.ginger.email@gmail.com>
2018-03-31 12:21:14 +0100

    improve user show layout

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,4 +3,9 @@ class UserController < ApplicationController
     @users = User.all
     erb :'users/index'
   end
+
+  get '/users/:id' do
+    @user = User.find(params[:id])
+    erb :'users/show'
+  end
 end
--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -29,4 +29,28 @@ describe 'UserController' do
       expect(page.body).to include("href=\"/users/#{@admin.id}\"")
     end
   end
+
+  describe '/users/:id' do
+    before do
+      @admin = User.create(name: 'Andy',
+                           username: 'andy',
+                           email: 'andy@admin.com',
+                           password: 'i@f0ub#zFJbb*XFV0ANn')
+      @write = User.create(name: 'Richy',
+                           username: 'rich',
+                           email: 'rich@write.com',
+                           password: 'Fug7tuff!e')
+      @read = User.create(name: 'Robbie',
+                          username: 'rob',
+                          email: 'rob@read.com',
+                          password: 'WiggleW@0p')
+    end
+
+    it 'has the users details' do
+      visit "/users/#{@admin.id}"
+      expect(page.body).to include(@admin.name)
+      expect(page.body).to include(@admin.username)
+      expect(page.body).to include(@admin.email)
+    end
+  end
 end
