1e04825c20dd8fd90e22ecfb11b1f85f7755687e
Robert Hughes <roberts.ginger.email@gmail.com>
2018-03-31 17:09:14 +0100

    edit user form

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,4 +8,16 @@ class UserController < ApplicationController
     @user = User.find(params[:id])
     erb :'users/show'
   end
+
+  patch '/users/:id' do
+    user = User.find(params[:id])
+    user.update(params[:user])
+    redirect "/user/#{user.id}"
+  end
+
+  get '/users/:id/edit' do
+    @user = User.find(params[:id])
+    @groups = Group.all
+    erb :'users/edit'
+  end
 end
