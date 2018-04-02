813af9d16492b2e6346440f6703a3be7bf88cb73
Robert Hughes <roberts.ginger.email@gmail.com>
2018-04-02 11:20:48 +0100

    complete show group

--- spec/controllers/group_controller_spec.rb
+++ spec/controllers/group_controller_spec.rb
@@ -23,4 +23,48 @@ describe 'GroupController' do
       expect(page.status_code).to eql(200)
     end
   end
+
+  describe '/groups/:id' do
+    before do
+      @admin = Group.create(name: 'admin', privilege: 'admin')
+      @hub = Device.create(serial_number: '123',
+                           model: 'HH6A',
+                           groups: [@admin])
+      @stb = Device.create(serial_number: '321',
+                           model: 'TLA',
+                           groups: [@admin])
+      @andy = User.create(name: 'Andy',
+                          username: 'andy',
+                          email: 'andy@admin.com',
+                          password: 'i@f0ub#zFJbb*XFV0ANn',
+                          group_id: @admin.id)
+      @richy = User.create(name: 'Richy',
+                           username: 'rich',
+                           email: 'rich@write.com',
+                           password: 'Fug7tuff!e',
+                           group_id: @admin.id)
+    end
+
+    it 'allows you to view an individual group' do
+      visit "/groups/#{@admin.id}"
+      expect(page.body).to include(@admin.name)
+      expect(page.body).to include(@admin.privilege)
+      expect(page.body).to include(@hub.serial_number)
+      expect(page.body).to include(@stb.serial_number)
+      expect(page.body).to include(@andy.name)
+      expect(page.body).to include(@richy.name)
+    end
+
+    it 'links to editing a group' do
+      visit "/groups/#{@admin.id}"
+      click_link 'edit'
+      expect(page.status_code).to eql(200)
+    end
+
+    it 'lets you delete a group' do
+      visit "/groups/#{@admin.id}"
+      click_button 'delete'
+      expect(Group.find_by(name: 'admin')).to eql(nil)
+    end
+  end
 end
